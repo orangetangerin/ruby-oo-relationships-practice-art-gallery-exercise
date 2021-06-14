a2e7335ef1bde6a0022d98d16f93b941e7a663b2
Jonathan Yu <jonyu02@gmail.com>
2021-06-14 09:32:21 -0400

    commit tree 3fb4b280ca54bc84df01af475d2a148f486514d0

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Artist
 
   attr_reader :name, :years_experience
@@ -5,6 +7,51 @@ class Artist
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+  end
+
+  def all
+    paintings = Painting.all.select do |painting|
+      painting.artist == self
+    end
+    paintings.map do |painting|
+      painting.artist
+    end
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    galleries = paintings.map do |painting|
+      painting.gallery
+    end
+    galleries.uniq
+  end
+
+  def cities
+    cities = galleries.map do |gallery|
+      gallery.city
+    end
+    cities.uniq
+  end
+
+  def total_experience
+    self.all.reduce(0) do |sum, artist|
+      sum + artist.years_experience
+    end
+  end
+
+  def most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.length / artist.years_experience
+    end
+  end
+    
+  def create_painting(title, price, gallery)
+    new_painting = Painting.new(title, price, self, gallery)
   end
 
 end
